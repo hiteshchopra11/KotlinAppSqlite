14a8654235a29f4fd25631c3b2c8f29d82f98d74
Hitesh Chopra <hiteshchopra11@gmail.com>
2020-07-25 06:25:32 +0530

    Added Firebase Authentication for login and registration and fixed some UI bugs.

--- app/src/main/java/com/example/loginapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/loginapp/Activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.loginapp;
+package com.example.loginapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import com.example.loginapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
